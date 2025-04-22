266d76d1cd8e749654ed303d6ad504c0cd7a18e9
Dmytro Minyaylo <dimon9660@gmail.com>
2025-04-22 19:24:33 +0300

    Add django.contrib.admin to settings.py

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -34,6 +34,7 @@ INTERNAL_IPS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
