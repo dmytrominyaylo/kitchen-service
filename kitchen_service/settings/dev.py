d773a5bbc26d0e55c94b13ffceb0f2d27fb69bb7
Dmytro Minyaylo <dimon9660@gmail.com>
2025-05-07 17:11:23 +0300

    Added ALLOWED_HOSTS for dev and prod environments

--- kitchen_service/settings/dev.py
+++ kitchen_service/settings/dev.py
@@ -3,7 +3,10 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Database
--- kitchen_service/settings/prod.py
+++ kitchen_service/settings/prod.py
@@ -7,7 +7,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'localhost',
+    'kitchen-service-1yyq.onrender.com',
 ]
 
 external_hostname = os.getenv('RENDER_EXTERNAL_HOSTNAME')
